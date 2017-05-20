b6b34a336477a4d2968333a7f997c21d0e0d9256
DESKTOP-I6DHT1V\bao <450215151@qq.com>
2017-05-20 21:52:02 +0800

    add bootstrap style

--- src/index.js
+++ src/index.js
@@ -13,70 +13,73 @@ class Text extends React.Component {
         this.state = {
             $$s: fromJS({
                 data: {
-                    focus: !0,
+                    focus: !1,
                     errorText: ''
                 }
             })
         };
     }
 
-    setIn($$s) {
+    _setIn($$s) {
         this.setState({$$s})
-    }
-
-    _change(e, $$data) {
-        const p = this.props;
-        var value = e.target.value;
-        var info = testInput(value, p);
-
-        $$data.set('errorText', info.errorText);
-
-        p.onChange({value, ...info});
     }
 
     _isWarn() {
         const $$data = from(this.state.$$s, ['data']);
         const p = this.props;
-        return p.errorText != '' || $$data.get('errorText') != '';
+        return p.errorText !== '' || $$data.get('errorText') !== '';
+    }
+
+    change(e, $$data) {
+        const p = this.props;
+        var value = e.target.value;
+        var info = testInput(value, p);
+        $$data.set('errorText', info.errorText);
+        p.onChange({value, ...info});
     }
 
     render() {
         const p = this.props;
-        const $$data = from(this.state.$$s, ['data'], $$newS => this.setIn($$newS));
+        const $$data = from(this.state.$$s, ['data'], $$newS => this._setIn($$newS));
         var isWarn = this._isWarn();
-
+        var rootClass = `${ css.combine('BhyTextFile', isWarn && '-warn', p.full && '-full', $$data.get('focus') && '-focus') }`;
         return (
-          <div className={ `${ css.combine('BhyTextFile', isWarn && '-warn', $$data.get('focus') && '-focus') } ${ p.className }`  }>
+          <div className={  `${ p.className } ${ rootClass }`  }>
               { p.label != '' && p.showLabel && (
-                <div>
-                    <label>
-                        { p.label }
-                    </label>
-                    <label>
-                        :
-                    </label>
+                <div className={ s['BhyTextFile-label'] } style={ {width: `${ p.labelWith }`} }>
+                    <p>
+                        <label className={ s['BhyTextFile-label-name'] }>
+                            { p.label }
+                        </label>
+                        <label className={ s['BhyTextFile-label-mao'] }>
+                            :
+                        </label>
+                    </p>
                 </div>
               ) }
 
-              <input
-                type={ p.type }
-                title={ p.title }
-                name={ p.name }
-                value={ p.value }
-                placeholder={ p.placeholder }
-                onChange={ e => this._change(e, $$data) }
-                onFocus={ e => $$data.set('focus', !0) }
-                onBlur={ e => $$data.set('focus', !1) }
-              />
+              <div className={ s['BhyTextFile-input'] } style={ {width: p.full ? '100%' : p.inputWith} }>
+                  <input
+                    type={ p.type }
+                    title={ p.title }
+                    name={ p.name }
+                    value={ p.value }
+                    placeholder={ p.placeholder }
+                    onChange={ e => this.change(e, $$data) }
+                    onFocus={ e => $$data.set('focus', !0) }
+                    onBlur={ e => $$data.set('focus', !1) }
+                  />
 
-              <div className={ s['BhyTextFile-line'] }>
-                  <span className={ s['BhyTextFile-line-focus'] }></span>
-                  <span className={ s['BhyTextFile-line-noFocus'] }></span>
+                  { isWarn && (
+                    <p className={ s['BhyTextFile-errorText'] }>{ p.errorText || $$data.get('errorText') }</p>
+                  ) }
               </div>
 
-              { isWarn && (
-                <p>{ p.errorText || $$data.get('errorText') }</p>
-              ) }
+              <div className={ s['BhyTextFile-line'] }>
+                  <span className={ s['BhyTextFile-line-focus'] }/>
+                  <span className={ s['BhyTextFile-line-noFocus'] }/>
+              </div>
+
           </div>
         );
     }
@@ -87,7 +90,6 @@ var cn = {
     rexErrorText: '[label]格式不对',
     numberErrorText: '[label]必须是数组',
     emptyErrorText: '[label]不可为空',
-    emailErrorText: '邮箱的格式不对',
 };
 
 var en = {
@@ -96,10 +98,10 @@ var en = {
     rexErrorText: '[label] format is wrong',
     numberErrorText: '[label] must is number',
     emptyErrorText: '[label]not allow empty',
-    emailErrorText: 'The mailbox is out of format',
 };
 Text.defaultProps = {
-    theme: 'theme1',
+    theme: 'border',
+    full: !1,
     max: 20,
     min: 2,
     label: '',
@@ -107,11 +109,14 @@ Text.defaultProps = {
     showLabel: !0,
     rexPassIsRight: !0,
     className: '',
+    inputWith: '200px',
     ...en,
 };
 
-
 Text.propTypes = {
+    labelWith: React.PropTypes.string,
+    inputWith: React.PropTypes.string,
+    full: React.PropTypes.bool,
     language: React.PropTypes.string,
     theme: React.PropTypes.string,
     label: React.PropTypes.string,
@@ -137,6 +142,7 @@ Text.propTypes = {
     errorText: React.PropTypes.string,
     showLabel: React.PropTypes.bool,
     className: React.PropTypes.string,
+
 };
 
 export default Text;
--- test/index.js
+++ test/index.js
@@ -29,73 +29,85 @@ class Main extends React.Component {
         const $$data = from(this.state.$$s, ['data'], $$newS => this.setIn($$newS));
 
         const $$cInput = $$data.get('cInput');
+        const labelWith = '110px'
         return (
-          <div>
-              <Text
-                label={ `username` }
-                type={ `text`}
-                max={ 16 }
-                min={ 5 }
-                value={ $$cInput.get('username') }
-                onChange={ ({value}) => $$cInput.set('username', value)  }
-              />
+          <div className="main">
+             <div className="main-inputList">
+                 <Text
+                   labelWith={ labelWith }
+                   label={ `username` }
+                   type={ `text`}
+                   max={ 16 }
+                   min={ 5 }
+                   value={ $$cInput.get('username') }
+                   onChange={ ({value}) => $$cInput.set('username', value)  }
+                 />
 
-              <Text
-                label={ `password` }
-                type={ `password`}
-                max={ 16 }
-                min={ 4 }
-                value={ $$cInput.get('password') }
-                onChange={ ({value}) => $$cInput.set('password', value)  }
-              />
+                 <Text
+                   labelWith={ labelWith }
+                   label={ `password` }
+                   type={ `password`}
+                   max={ 16 }
+                   min={ 4 }
+                   placeholder={ '密码' }
+                   value={ $$cInput.get('password') }
+                   onChange={ ({value}) => $$cInput.set('password', value)  }
+                 />
 
-              <Text
-                label={ `user phone` }
-                type={ `text` }
-                value={ $$cInput.get('phone') }
-                max={ null }
-                min={ null }
-                rex={ /^\d{4,18}$/ }
-                rexErrorText={ '[label] error !!!' }
-                onChange={ ({value}) => $$cInput.set('phone', value)  }
-              />
+                 <Text
+                   labelWith={ labelWith }
+                   label={ `user phone` }
+                   type={ `text` }
+                   value={ $$cInput.get('phone') }
+                   max={ null }
+                   min={ null }
+                   rex={ /^\d{4,18}$/ }
+                   rexErrorText={ '[label] error !!!' }
+                   onChange={ ({value}) => $$cInput.set('phone', value)  }
+                 />
 
-              <Text
-                label={ `your gmail` }
-                type={ `text` }
-                value={ $$cInput.get('email') }
-                rexs={ [
-                    {
-                        rex: /^.+@.+\..+$/, passIsRight: !0,
-                    },
-                    {
-                        rex: /^.+@outlook\.com$/, passIsRight: !1, errorText: 'not allow outlook',
-                    },
-                    {
-                        rex: /^.+@gmail\.com$/, passIsRight: !0, errorText: '[label] must be gmail',
-                    }
-                ]}
-                onChange={ ({value}) => $$cInput.set('email', value)  }
-              />
+                 <Text
+                   labelWith={ labelWith }
+                   label={ `your gmail` }
+                   type={ `text` }
+                   value={ $$cInput.get('email') }
+                   rexs={ [
+                       {
+                           rex: /^.+@.+\..+$/, passIsRight: !0,
+                       },
+                       {
+                           rex: /^.+@outlook\.com$/, passIsRight: !1, errorText: 'not allow outlook',
+                       },
+                       {
+                           rex: /^.+@gmail\.com$/, passIsRight: !0, errorText: '[label] must be gmail',
+                       }
+                   ]}
+                   onChange={ ({value}) => $$cInput.set('email', value)  }
+                 />
 
-              <Text
-                label={ `password1` }
-                type={ `password1`}
-                max={ 16 }
-                min={ 4 }
-                value={ $$cInput.get('password1') }
-                onChange={ ({value}) => $$cInput.set('password1', value)  }
-              />
-              <Text
-                label={ `password2` }
-                type={ `password2`}
-                value={ $$cInput.get('password2') }
-                errorText={
-                    $$cInput.get('password2') != $$cInput.get('password1') ?
-                      `password2 must equal password1` : ''
-                }
-                onChange={ ({value}) => $$cInput.set('password2', value)  }
-              />
+                 <Text
+                   labelWith={ labelWith }
+                   label={ `password1` }
+                   type={ `password`}
+                   max={ 16 }
+                   min={ 4 }
+                   value={ $$cInput.get('password1') }
+                   onChange={ ({value}) => $$cInput.set('password1', value)  }
+                 />
+
+                 <Text
+                   labelWith={ labelWith }
+                   label={ `password2` }
+                   type={ `password`}
+                   full
+                   value={ $$cInput.get('password2') }
+                   errorText={
+                       $$cInput.get('password2') != $$cInput.get('password1') ?
+                         `password2 must equal password1` : ''
+                   }
+                   onChange={ ({value}) => $$cInput.set('password2', value)  }
+                 />
+             </div>
 
           </div>
         )
